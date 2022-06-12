47393076a59abb902cf8db372baa79fbfb2cf456
Janek Rahrt <janek.rahrt@me.com>
2022-06-12 11:05:51 +0200

    feat: commit mainnet secrets

--- wallet.ts
+++ wallet.ts
@@ -1,15 +1,20 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useMemo, useState } from "react";
-import { Account, defaultProvider, ec, KeyPair } from "starknet";
+import { Account, Provider, ec, KeyPair } from "starknet";
+
+const provider = new Provider({ network: "mainnet-alpha" });
 
 export const useWallet = () => {
   const [keyPair, setKeyPair] = useState<KeyPair>();
   const [reloadTrigger, setReloadTrigger] = useState(false);
+
   useEffect(() => {
     setReloadTrigger(false);
+
     if (reloadTrigger) {
       setKeyPair(undefined);
     }
+
     // load keypair from storage or generate new one
     AsyncStorage.getItem("secret").then(async (secret) => {
       if (!keyPair) {
@@ -28,10 +33,11 @@ export const useWallet = () => {
     }
     return ec.getStarkKey(keyPair);
   }, [keyPair]);
+
   const address = useMemo(() => {
     // return calculateContractAddress();
     return publicKey
-      ? "0x002fc8867e8756705985fd2054d3145d609b255b298fe7d28aef10d1f96d0e6e"
+      ? "0x0132f9c9fc91fbcf7552f80f2446a6ed814f8ee07dde4935d06f234c5f30d74d"
       : undefined;
   }, [publicKey]);
 
@@ -39,7 +45,7 @@ export const useWallet = () => {
     if (!address || !keyPair) {
       return;
     }
-    return new Account(defaultProvider, address, keyPair);
+    return new Account(provider, address, keyPair);
   }, [address, keyPair]);
 
   return {
